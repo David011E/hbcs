e9de5e6401ef0a92d19acb31d096926bf226b219
David011e <davidoliver011e@gmail.com>
2024-02-04 11:51:19 +0000

    Added secret key to env file

--- hbcs/settings.py
+++ hbcs/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+cx*)a(005g@o24@ip9bwtylu2q35f5b^*o%98x4le6kr1qb8c'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
