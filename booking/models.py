521a8fbe44fcfc508947a3ffe093e2840fa82cba
David011e <davidoliver011e@gmail.com>
2024-02-05 12:47:29 +0000

    Added Booking models

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Booking(models.Model):
+    STATUS_CHOICES = [
+        (0, 'Available'),
+        (1, 'Booked'),
+        (2, 'Canceled'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Assuming you have a User model
+    date = models.DateField()
+    time = models.TimeField()
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+
+    def __str__(self):
+        return f"{self.user}'s booking on {self.date} at {self.time} - {self.get_status_display()}"
