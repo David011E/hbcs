5b7a277cce79860dbd698b837d239fccec5717d8
David011e <davidoliver011e@gmail.com>
2024-02-07 16:25:10 +0000

    Updated and used SummernoteModel for booking

--- booking/admin.py
+++ booking/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Booking
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Booking)
+class Booking(SummernoteModelAdmin):
+    list_display = ('user', 'date' , 'time', 'status')
+    list_filter = ['status']
+    summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(Booking)
