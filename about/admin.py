e8cf38f16b14ea9ea1a4f85c2c99b822dfa39499
David011e <davidoliver011e@gmail.com>
2024-02-07 12:42:42 +0000

    Added summernote to the project

--- about/admin.py
+++ about/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Reviews
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Reviews)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'author', 'content', 'created_on')
+    search_fields = ['title', 'author']
+    list_filter = ('created_on', 'author')
+    summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(Reviews)
--- hbcs/urls.py
+++ hbcs/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path("", include("home.urls"), name='home-urls'),
     path("booking/", include("booking.urls"), name='booking-urls'),  
     path("about/", include("about.urls"), name='about-urls'),  # Add a trailing slash
