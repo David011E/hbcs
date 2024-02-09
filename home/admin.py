1232b454bd0c9cf8000000a4f18bdb5f6683f097
David011e <davidoliver011e@gmail.com>
2024-02-09 13:39:44 +0000

    Added Image models and installed cloudinary

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Image
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+@admin.register(Image)
+class Image(SummernoteModelAdmin):
+    list_display = ('featured_image', 'description', 'uploaded_at')
+    list_filter = ['uploaded_at']
+    summernote_fields = ['content']
--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Image(models.Model):
+    featured_image = CloudinaryField('image', default='placeholder')
+    description = models.CharField(max_length=200, blank=True, null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
