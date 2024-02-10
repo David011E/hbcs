5afa9c238586f766555f8df833c0d28eecd88061
David011e <davidoliver011e@gmail.com>
2024-02-10 13:15:22 +0000

    Styled the home page and adjusted the views for it

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Image
 
 # Create your views here.
 def my_home(request):
+    image_list = Image.objects.filter(description__icontains='main')
+    cert_image_list = Image.objects.filter(description__icontains='cert')
+    template_name = "home/index.html"
+
     return render(
-        request,
-        "home/index.html",
-    )
+        request, 
+        template_name, 
+        {'image_list': image_list, 'cert_image_list': cert_image_list}
+        )
