893c87b436325934528d67f05ee8ecf6ead51bfc
David011e <davidoliver011e@gmail.com>
2024-02-11 13:34:38 +0000

    Added About us data fro m json and styled the page

--- about/urls.py
+++ about/urls.py
@@ -1,7 +1,8 @@
-from .views import ReviewViews
+from .views import ReviewViews, my_json_view
 from django.urls import path
 
 urlpatterns = [
     path('', ReviewViews.as_view(), name='about'),
+    path('my-json-view/', my_json_view, name='my_json_view'),
     # other URL patterns
 ]
--- about/views.py
+++ about/views.py
@@ -1,8 +1,38 @@
+import os
+import json
 from django.shortcuts import render
 from django.views import generic
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
 from .models import Reviews
 
 # Create your views here.
 class ReviewViews(generic.ListView):
     queryset = Reviews.objects.all()
     template_name = "about/about.html"
+
+    def get_context_data(self):
+        context = super().get_context_data()
+
+        # Add the JSON data to the context
+        json_file_path = os.path.join(settings.BASE_DIR, 'data', 'company.json')
+        with open(json_file_path, 'r') as json_file:
+            data = json.load(json_file)
+
+        context['company'] = data
+
+        return context
+
+@csrf_exempt
+def my_json_view(request):
+    data = []
+
+    # Path to your JSON file
+    json_file_path = os.path.join(settings.BASE_DIR, 'data', 'company.json')
+
+    # Read JSON data from the file
+    with open(json_file_path, 'r') as json_file:
+        data = json.load(json_file)
+
+    return JsonResponse(data, safe=False)
