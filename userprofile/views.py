24f3e499ced9e37203f078ccbc6c06a11d79d280
David011e <davidoliver011e@gmail.com>
2024-02-21 11:50:04 +0000

    filterd the reviews for users to see the reviews made by them

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from about.models import Reviews
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required # Ensures that only logged-in users can access this view
 def user_profile(request):
     template_name = "userProfile/user_profile.html"
 
-    return render(
-        request,
-        template_name,
-    )
+    # Filters reviews to only those created by the logged-in user
+    reviews = Reviews.objects.filter(author=request.user)
+    return render(request, template_name, {'reviews': reviews})
